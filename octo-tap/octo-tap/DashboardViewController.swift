8dd6c56915b86cecf9f022770018f85ef2defd92
Lukas Valine <lukas@valine.io>
2017-07-18 20:48:06 -0500

    Initial commit with Xcode project

---
+++ octo-tap/octo-tap/DashboardViewController.swift
@@ -0,0 +1,32 @@
+//	This file is part of Octo Tap.
+//
+//	Octo Tap is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	Octo Tap is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with Octo Tap.  If not, see <http://www.gnu.org/licenses/>.
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
