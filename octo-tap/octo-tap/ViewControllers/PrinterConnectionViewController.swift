c9f1e0e7f14935c35993c1c689b89e4ec45c47d8
Lukas Valine <lukas@valine.io>
2017-08-12 20:07:58 -0500

    Working on connection screen viewcontroller layout

---
+++ octo-tap/octo-tap/ViewControllers/PrinterConnectionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PrinterConnectionViewController.swift
+//  octo-tap
+//
+//  Created by Lukas Valine on 8/12/17.
+//  Copyright © 2017 Lukas Valine. All rights reserved.
+//
+
+import UIKit
+
+
+class PrinterConnectionViewController: UITableViewController {
+	
+	override func viewDidLoad() {
+		
+		
+		
+	}
+	
+	
+	
+	
+}
